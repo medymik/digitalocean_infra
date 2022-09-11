327679c9da66af22226c6869a1a79ae3a04b7207
medymik <mohammed.ymik@outlook.com>
2022-09-11 15:19:59 +0100

    provide space secret and access id

--- providers.tf
+++ providers.tf
@@ -8,7 +8,10 @@ terraform {
 }
 
 variable "do_token" {}
-
+variable "spaces_access_id" {}
+variable "spaces_secret_key" {}
 provider "digitalocean" {
 	token = var.do_token
+  spaces_access_id = var.spaces_access_id
+  spaces_secret_key = var.spaces_secret_key
 }
